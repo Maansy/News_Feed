766e021746be51eaf673d02abfa626fbe30817d3
Maansy <abdomansy19@gmail.com>
2023-12-01 14:48:01 +0200

    Add ERD and create the tables

---
+++ create_newsfeed_tables.sql
@@ -0,0 +1,55 @@
+-- Create the User table
+CREATE TABLE IF NOT EXISTS User (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Create the Post table
+CREATE TABLE IF NOT EXISTS Post (
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Create the Comment table
+CREATE TABLE IF NOT EXISTS Comment (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES Post(post_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Create the Like table
+CREATE TABLE IF NOT EXISTS Likes (
+    like_id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES Post(post_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Create the Share table
+CREATE TABLE IF NOT EXISTS Share (
+    share_id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES Post(post_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Create the Follow/Friendship table
+CREATE TABLE IF NOT EXISTS Follow (
+    follower_id INT NOT NULL,
+    followed_id INT NOT NULL,
+    PRIMARY KEY (follower_id, followed_id),
+    FOREIGN KEY (follower_id) REFERENCES User(user_id),
+    FOREIGN KEY (followed_id) REFERENCES User(user_id)
+);
